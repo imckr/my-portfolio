6e5dd35ea22d2081370170778b2e820c139fcc05
Chandra Kumar Rajwal <122195878+imckr@users.noreply.github.com>
2024-09-21 00:43:11 +0530

    under development tagged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,33 @@
-import { Jet } from "@/Fonts/font";
-import Marquee from "@/components/Marquee"
-import Margins from "@/components/Margins"
+import { Jet, inter_bold } from "@/Fonts/font";
+import Marquee from "@/components/Marquee";
+import Margins from "@/components/Margins";
 import Slogan from "@/components/Slogan";
 import PageLabel from "@/components/PageLabel";
 import Screen from "@/components/Screen";
+
 
 export default function Home() {
 
   return (
     <>
-      <Margins/>
-      <Slogan />
-      <PageLabel/>
-      <Screen/>
-      <div className={`upper ${Jet.className} text-sm absolute flex overflow-hidden z-[-1%] ml-[4%] w-[88%] top-[-1%]`}>
-      <Marquee />
+      <div className={`absolute top-[60%] left-[32%] ${inter_bold.className} text-4xl blinking`}>
+        <h1>{"// Under Development Available Soon..."}</h1>
       </div>
-      <div className={`upper ${Jet.className} text-sm absolute flex overflow-hidden z-[-1%] w-[88%] bottom-[1%] right-[4%]`}>
-      <Marquee />
+      <Margins />
+      <Slogan />
+      <PageLabel />
+      <Screen />
+
+      <div
+        className={`upper ${Jet.className} text-sm absolute flex overflow-hidden z-[-1%] ml-[4%] w-[88%] top-[-1%]`}
+      >
+        <Marquee />
+      </div>
+
+      <div
+        className={`upper ${Jet.className} text-sm absolute flex overflow-hidden z-[-1%] w-[88%] bottom-[1%] right-[4%]`}
+      >
+        <Marquee />
       </div>
     </>
   );
