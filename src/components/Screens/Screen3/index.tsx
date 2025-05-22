a3f7be789978e6c493c06de11ee3a42cb6111d24
Chandra Kumar Rajwal <122195878+imckr@users.noreply.github.com>
2025-05-22 22:49:19 +0530

    ...

--- src/components/Screens/Screen3/index.tsx
+++ src/components/Screens/Screen3/index.tsx
@@ -18,11 +18,11 @@ export default function Screen3() {
             title: "LANGUAGES I KNOW :",
             width: "w-36",
             img: [
-                "./images/skill/Cpp.svg",
-                "./images/skill/Node.svg",
-                "./images/skill/java.svg",
-                "./images/skill/Rust.svg",
-                "./images/skill/python.svg",
+                "./images/Cpp.svg",
+                "./images/Node.svg",
+                "./images/java.svg",
+                "./images/Rust.svg",
+                "./images/python.svg",
             ],
         },
 
